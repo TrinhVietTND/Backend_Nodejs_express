2234c640568811429b18d7e5347d1aedfd557438
TND <t2vjet90@gmail.com>
2023-04-15 16:31:00 +0700

    your message

--- src/server.js
+++ src/server.js
@@ -1,43 +1,13 @@
 import express from 'express';
-import configViewEngine from './configs/viewEngine';
-import initWebRoute from './route/web';
-import initAPIRoute from './route/api';
-// import connection from './configs/connectDB';
-
+import configViewEngine from './configs/viewEngine'
 require('dotenv').config();
-var morgan = require('morgan')
-
-
-
 const app = express();
 const port = process.env.PORT || 8080;
+console.log('>>> check port:', port);
 
-app.use((req, res, next) => {
-    //check => return res.send()
-    console.log('>>> run into my middleware')
-    console.log(req.method)
-    next();
+app.get('/',(req,res)=> {
+    res.render('index.ejs');
 })
-
-app.use(morgan('combined'))
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// setup view engine
-configViewEngine(app);
-
-//init web route
-initWebRoute(app);
-
-// init api route
-initAPIRoute(app);
-
-//handle 404 not found
-app.use((req, res) => {
-    return res.render('404.ejs')
-})
-
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
